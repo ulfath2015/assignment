96c8f57b4da13bae8486c2d204f48356473edd92
ultafa <ultafa@PRINHYLTPDL0530.(none)>
2015-04-22 10:38:24 +0530

    reorganising directory structure
    Change-Id: Ie60d793502662e8507c5d7a7e0cfcf7f4898305f

---
+++ src/main/java/com/imaginea/assignment/ComparatorService.java
@@ -0,0 +1,12 @@
+package com.imaginea.assignment;
+
+import java.util.Set;
+
+public interface ComparatorService {
+
+	public Set<String> findMatchingStrings();
+	public boolean compare(String sourceName);
+	
+	
+
+}
---
+++ src/main/java/com/imaginea/assignment/FileComparatorRunner.java
@@ -0,0 +1,11 @@
+package com.imaginea.assignment;
+
+public class FileComparatorRunner {
+	
+	public static void main(String args[]){
+		
+		FileComparator fileComparator  = new FileComparator("FileNames.properties");
+		fileComparator.findMatchingStrings();
+	}
+
+}
